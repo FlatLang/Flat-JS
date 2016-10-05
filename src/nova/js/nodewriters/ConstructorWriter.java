46fc08901306bfb0535fba50933000b23187b409
BSteffaniak <BradenSteffaniak@gmail.com>
2016-10-04 23:45:42 -0700

    Updated to assign constructors to the novaConstructors object.

--- src/nova/js/nodewriters/ConstructorWriter.java
+++ src/nova/js/nodewriters/ConstructorWriter.java
@@ -6,5 +6,9 @@ public abstract class ConstructorWriter extends BodyMethodDeclarationWriter
 {
 	public abstract Constructor node();
 	
-	
+	@Override
+	public StringBuilder writeAssignedVariable(StringBuilder builder)
+	{
+		return builder.append("novaConstructors.new").append(writeName());
+	}
 }
