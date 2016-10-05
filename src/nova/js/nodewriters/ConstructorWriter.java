88cc875a5ce6ec6e194daa4b17cd0926e61ea757
BSteffaniak <BradenSteffaniak@gmail.com>
2016-10-05 12:15:52 -0700

    Overwrote writeBody function.

--- src/nova/js/nodewriters/ConstructorWriter.java
+++ src/nova/js/nodewriters/ConstructorWriter.java
@@ -7,6 +7,23 @@ public abstract class ConstructorWriter extends BodyMethodDeclarationWriter
 	public abstract Constructor node();
 	
 	@Override
+	public StringBuilder writeBody(StringBuilder builder)
+	{
+		builder.append("{\n");
+		
+		getWriter(node().getScope()).write(builder, false, true);
+		
+		ClassDeclaration extended = node().getDeclaringClass().getExtendedClassDeclaration();
+		
+		if (extended != null)
+		{
+			builder.append(extended.getName()).append(".call(this);\n");
+		}
+		
+		return builder.append('}');
+	}
+	
+	@Override
 	public StringBuilder writeAssignedVariable(StringBuilder builder)
 	{
 		return builder.append("novaConstructors.new").append(writeName());
