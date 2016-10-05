9dfaa2c606e2b0f3e3aaeffaee47c8ea1a685e47
BSteffaniak <BradenSteffaniak@gmail.com>
2016-10-05 12:18:06 -0700

    Added support for -local-scope argument.

--- src/nova/js/nodewriters/ProgramWriter.java
+++ src/nova/js/nodewriters/ProgramWriter.java
@@ -1,6 +1,7 @@
 package nova.js.nodewriters;
 
 import net.fathomsoft.nova.tree.*;
+import nova.js.engines.JSCompileEngine;
 
 public abstract class ProgramWriter extends NodeWriter
 {
@@ -9,11 +10,23 @@ public abstract class ProgramWriter extends NodeWriter
 	@Override
 	public StringBuilder write(final StringBuilder builder)
 	{
+		boolean localScope = ((JSCompileEngine)node().getProgram().getController().compileEngine).localScope;
+		
+		if (localScope)
+		{
+			builder.append("(function () {\n");
+		}
+		
 		builder.append("var novaConstructors = {};\n\n");
 		
 		node().forEachChild(child -> {
 			getWriter(child).write(builder);
 		});
+		
+		if (localScope)
+		{
+			builder.append("})();");
+		}
 		
 		return builder;
 	}
