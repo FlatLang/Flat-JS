8be16fa4c9fc904f146c775ede367dae166d7a18
BSteffaniak <BradenSteffaniak@gmail.com>
2016-10-05 22:43:58 -0700

    Added writeArrayAccess functions.

--- src/nova/js/nodewriters/ValueWriter.java
+++ src/nova/js/nodewriters/ValueWriter.java
@@ -6,5 +6,18 @@ public abstract class ValueWriter extends NodeWriter
 {
 	public abstract Value node();
 	
+	public StringBuilder writeArrayAccess()
+	{
+		return writeArrayAccess(new StringBuilder());
+	}
 	
+	public StringBuilder writeArrayAccess(StringBuilder builder)
+	{
+		if (node().arrayAccess != null)
+		{
+			return getWriter(node().arrayAccess).writeExpression(builder);
+		}
+		
+		return builder;
+	}
 }
