f1d8d1a7ce9bcdbe1de41fc98d101b393103be49
BSteffaniak <BradenSteffaniak@gmail.com>
2016-10-05 23:46:02 -0700

    Updated to write classes in hierarchical order.

--- src/nova/js/nodewriters/ProgramWriter.java
+++ src/nova/js/nodewriters/ProgramWriter.java
@@ -3,9 +3,33 @@ package nova.js.nodewriters;
 import net.fathomsoft.nova.tree.*;
 import nova.js.engines.JSCompileEngine;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public abstract class ProgramWriter extends TypeListWriter
 {
 	public abstract Program node();
+	
+	public ClassDeclaration[] getClassesHierarchicalInOrder()
+	{
+		final ArrayList<ClassDeclaration> list = new ArrayList<>();
+		
+		node().forEachVisibleListChild(file -> {
+			Arrays.stream(file.getClassDeclarations()).forEach(c -> {
+				ClassDeclaration[] hierarchy = c.getClassHierarchy(true, true);
+				
+				for (int i = hierarchy.length - 1; i >= 0; i--)
+				{
+					if (!list.contains(hierarchy[i]))
+					{
+						list.add(hierarchy[i]);
+					}
+				}
+			});
+		});
+		
+		return list.toArray(new ClassDeclaration[0]);
+	}
 	
 	@Override
 	public StringBuilder write(final StringBuilder builder)
@@ -19,9 +43,10 @@ public abstract class ProgramWriter extends TypeListWriter
 		
 		builder.append("var novaConstructors = {};\n\n");
 		
-		node().forEachChild(child -> {
+		for (ClassDeclaration child : getClassesHierarchicalInOrder())
+		{
 			getWriter(child).write(builder);
-		});
+		}
 		
 		if (localScope)
 		{
