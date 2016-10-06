f3e5929972438b446a59196b673e111abef7329c
BSteffaniak <BradenSteffaniak@gmail.com>
2016-10-05 23:54:38 -0700

    Implemented Super output.

--- src/nova/js/nodewriters/SuperWriter.java
+++ src/nova/js/nodewriters/SuperWriter.java
@@ -1,10 +1,22 @@
 package nova.js.nodewriters;
 
+import net.fathomsoft.nova.tree.MethodCall;
 import net.fathomsoft.nova.tree.variables.Super;
 
 public abstract class SuperWriter extends PriorityWriter
 {
 	public abstract Super node();
 	
-	
+	@Override
+	public StringBuilder writeExpression(StringBuilder builder)
+	{
+		builder.append("this.");
+		
+		if (node().getAccessedNode() instanceof MethodCall)
+		{
+			builder.append("super_");
+		}
+		
+		return builder.append(writeAccessedExpression(false));
+	}
 }
