a91130f85a1358a5305099edba8487d48ad85a4a
BSteffaniak <BradenSteffaniak@gmail.com>
2016-10-05 22:40:32 -0700

    Implemented exception handling output.

--- src/nova/js/nodewriters/CatchWriter.java
+++ src/nova/js/nodewriters/CatchWriter.java
@@ -1,10 +1,26 @@
 package nova.js.nodewriters;
 
 import net.fathomsoft.nova.tree.exceptionhandling.Catch;
+import net.fathomsoft.nova.tree.exceptionhandling.ExceptionHandler;
 
 public abstract class CatchWriter extends ExceptionHandlerWriter
 {
 	public abstract Catch node();
 	
-	
+	@Override
+	public StringBuilder write(StringBuilder builder)
+	{
+		builder.append("catch (").append(node().getExceptionDeclaration().getName()).append(") ").append(getWriter(node().getScope()).write(true, false));
+		
+		if (node().getNextNode() instanceof ExceptionHandler)
+		{
+			builder.append(' ');
+		}
+		else
+		{
+			builder.append('\n');
+		}
+		
+		return builder;
+	}
 }
