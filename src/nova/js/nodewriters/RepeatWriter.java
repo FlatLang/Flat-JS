3480640fa5f32611435884eb8e935d5f880715a9
BSteffaniak <BradenSteffaniak@gmail.com>
2016-10-05 22:43:09 -0700

    Implemented Repeat output.

--- src/nova/js/nodewriters/RepeatWriter.java
+++ src/nova/js/nodewriters/RepeatWriter.java
@@ -6,5 +6,24 @@ public abstract class RepeatWriter extends LoopWriter
 {
 	public abstract Repeat node();
 	
-	
+	@Override
+	public StringBuilder write(StringBuilder builder)
+	{
+		Value value = node().getValueNode();
+		
+		if (value != null)
+		{
+			builder.append("for (").append(node().getName()).append(" = 0; ").append(node().getName()).append(" < ").append(getWriter(value).writeExpression()).append("; ").append(node().getName()).append("++) ");
+		}
+		else
+		{
+			builder.append("for (;;) ");
+		}
+		
+		Scope scope = node().getScope();
+		
+		getWriter(scope).write(builder);
+		
+		return builder;
+	}
 }
