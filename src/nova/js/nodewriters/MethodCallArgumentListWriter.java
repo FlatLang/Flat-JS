c4fa836a54aa7745e68647667a1280f2df0d88f5
BSteffaniak <BradenSteffaniak@gmail.com>
2016-10-10 02:21:57 -0700

    Implemented write function required for writing named arguments in order.

--- src/nova/js/nodewriters/MethodCallArgumentListWriter.java
+++ src/nova/js/nodewriters/MethodCallArgumentListWriter.java
@@ -6,5 +6,35 @@ public abstract class MethodCallArgumentListWriter extends ArgumentListWriter
 {
 	public abstract MethodCallArgumentList node();
 	
-	
+	@Override
+	public StringBuilder write(StringBuilder builder, boolean parenthesis)
+	{
+		MethodCall call = node().getMethodCall();
+		
+		CallableMethod method = call.getInferredDeclaration();
+		
+		Value[] values = method instanceof NovaMethodDeclaration ? node().getArgumentsInOrder((NovaMethodDeclaration)method) : node().getArgumentsInOrder();
+		
+		if (parenthesis)
+		{
+			builder.append('(');
+		}
+		
+		for (int i = 0; i < values.length; i++)
+		{
+			if (i > 0)
+			{
+				builder.append(", ");
+			}
+			
+			getWriter(values[i]).writeExpression(builder);
+		}
+		
+		if (parenthesis)
+		{
+			builder.append(')');
+		}
+		
+		return builder;
+	}
 }
