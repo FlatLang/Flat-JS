fa1e817d92845586e802ca010279082df2eb58ea
BSteffaniak <BradenSteffaniak@gmail.com>
2016-10-09 21:07:18 -0700

    Updated write functions.

--- src/nova/js/nodewriters/InitializationMethodWriter.java
+++ src/nova/js/nodewriters/InitializationMethodWriter.java
@@ -1,10 +1,29 @@
 package nova.js.nodewriters;
 
+import net.fathomsoft.nova.tree.ClassDeclaration;
 import net.fathomsoft.nova.tree.InitializationMethod;
 
 public abstract class InitializationMethodWriter extends BodyMethodDeclarationWriter
 {
 	public abstract InitializationMethod node();
+	
+	@Override
+	public StringBuilder writePrototypeAccess(StringBuilder builder)
+	{
+		return builder;
+	}
+	
+	@Override
+	public StringBuilder writePrototypeAssignment(StringBuilder builder, ClassDeclaration clazz, boolean superCall)
+	{
+		return builder;
+	}
+	
+	@Override
+	public StringBuilder writeSuperName(StringBuilder builder)
+	{
+		return writeName(builder);
+	}
 	
 	@Override
 	public StringBuilder writeName(StringBuilder builder)
