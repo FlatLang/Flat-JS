30772d52da50d3e29b0778f777e3c819276dbcc6
Braden Steffaniak <BradenSteffaniak@gmail.com>
2022-06-29 22:00:52 -0400

    Add method chaining operator functionality

--- src/nova/js/nodewriters/MethodCallWriter.java
+++ src/nova/js/nodewriters/MethodCallWriter.java
@@ -1,6 +1,7 @@
 package nova.js.nodewriters;
 
 import net.fathomsoft.nova.tree.*;
+import net.fathomsoft.nova.tree.annotations.AsyncAnnotation;
 import net.fathomsoft.nova.tree.variables.ObjectReference;
 
 public abstract class MethodCallWriter extends VariableWriter
@@ -23,49 +24,58 @@ public abstract class MethodCallWriter extends VariableWriter
 
 		writeUsePrefix(builder);
 
-		if (callable instanceof MethodDeclaration)
-		{
-			if (node().isSuperCall())
-			{
-				getWriter((NovaMethodDeclaration)callable).writeSuperName(builder);
-			}
-			else
-			{
-				getWriter((MethodDeclaration)callable).writeName(builder);
-			}
-		}
-		else
-		{
-			builder.append(node().getName());
-		}
-		
-		if (callable instanceof InitializationMethod)
-		{
-			builder.append(".call(");
+		if (node().isChainNavigation()) {
 
-			if (node().getParentMethod() instanceof Constructor) {
-				builder.append("__value");
+			builder.append("__chain");
+
+			if (((Node)callable).containsAnnotationOfType(AsyncAnnotation.class)) {
+				builder.append("Async");
+			}
+
+			builder.append("('").append(writeName()).append("', [");
+			getWriter(node().getArgumentList()).write(builder, false).append("])");
+		} else {
+			writeName(builder);
+
+			if (callable instanceof InitializationMethod) {
+				builder.append(".call(");
+
+				if (node().getParentMethod() instanceof Constructor) {
+					builder.append("__value");
+				} else {
+					builder.append("this");
+				}
+
+				if (node().getArgumentList().getNumVisibleChildren() > 0) {
+					builder.append(", ");
+				}
+
+				return getWriter(node().getArgumentList()).write(builder, false).append(')');
 			} else {
-				builder.append("this");
+				getWriter(node().getArgumentList()).write(builder);
 			}
-			
-			if (node().getArgumentList().getNumVisibleChildren() > 0)
-			{
-				builder.append(", ");
-			}
-			
-			return getWriter(node().getArgumentList()).write(builder, false).append(')');
 		}
-		else
-		{
-			getWriter(node().getArgumentList()).write(builder);
-		}
-		
+
 		writeArrayAccess(builder);
 
 //		writeNullFallbackPostfix(builder);
 
 		return builder;
+	}
+
+	@Override
+	public StringBuilder writeName(StringBuilder builder) {
+		CallableMethod callable = node().getCallableDeclaration();
+
+		if (callable instanceof MethodDeclaration) {
+			if (node().isSuperCall()) {
+				return getWriter((NovaMethodDeclaration) callable).writeSuperName(builder);
+			} else {
+				return getWriter((MethodDeclaration) callable).writeName(builder);
+			}
+		} else {
+			return builder.append(node().getName());
+		}
 	}
 
 	@Override
