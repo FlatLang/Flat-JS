2f23ec3d6ba776cd77930237df8dc8b2dd44f6c9
Braden Steffaniak <BradenSteffaniak@gmail.com>
2022-07-13 13:48:13 -0400

    Implement continue

--- src/org/flatlang/js/nodewriters/ContinueWriter.java
+++ src/org/flatlang/js/nodewriters/ContinueWriter.java
@@ -5,6 +5,14 @@ import org.flatlang.tree.*;
 public abstract class ContinueWriter extends NodeWriter
 {
 	public abstract Continue node();
-	
-	
+
+	@Override
+	public StringBuilder writeExpression(StringBuilder builder) {
+		return builder.append("continue");
+	}
+
+	@Override
+	public StringBuilder write(StringBuilder builder) {
+		return writeExpression(builder).append(";\n");
+	}
 }
